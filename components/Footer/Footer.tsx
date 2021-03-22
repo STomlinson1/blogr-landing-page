2dfd2cfa7e5789098a752b5e54452cbb83410929
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-03-22 13:19:39 -0400

    Footer component and styles.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,10 +1,70 @@
 import styles from './Footer.module.scss';
+import Image from 'next/image';
 
 const Footer = () => {
 	return (
 		<footer className={styles.Footer}>
 			<div className={styles.container}>
-				<h1>My footer</h1>
+				<div className={styles.logo}>
+					<Image
+						src="/images/logo.svg"
+						alt="Blogr logo"
+						width={85}
+						height={35}
+						layout="responsive"
+					/>
+				</div>
+				<ul role="list">
+					<li>
+						<a>Product</a>
+					</li>
+					<li>
+						<a>Overview</a>
+					</li>
+					<li>
+						<a>Pricing</a>
+					</li>
+					<li>
+						<a>Marketplace</a>
+					</li>
+					<li>
+						<a>Features</a>
+					</li>
+					<li>
+						<a>Integrations</a>
+					</li>
+				</ul>
+				<ul role="list">
+					<li>
+						<a>Company</a>
+					</li>
+					<li>
+						<a>About</a>
+					</li>
+					<li>
+						<a>Team</a>
+					</li>
+					<li>
+						<a>Blog</a>
+					</li>
+					<li>
+						<a>Careers</a>
+					</li>
+				</ul>
+				<ul role="list">
+					<li>
+						<a>Connect</a>
+					</li>
+					<li>
+						<a>Contact</a>
+					</li>
+					<li>
+						<a>Newsletter</a>
+					</li>
+					<li>
+						<a>LinkedIn</a>
+					</li>
+				</ul>
 			</div>
 		</footer>
 	);
