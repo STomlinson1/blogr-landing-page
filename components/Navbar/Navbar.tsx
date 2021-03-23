d382d6bfeadd5ea4b463fec35960bc2719c65031
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-03-22 20:08:07 -0400

    Fix Nav styles.

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -22,36 +22,50 @@ const Navbar = () => {
 
 	return (
 		<nav className={`${styles.Navbar} ${scrollPosition > 350 ? styles.active : ''}`}>
-			<div className={styles.logo}>
-				<Image
-					src="/images/logo.svg"
-					alt="Blogr logo"
-					width={85}
-					height={35}
-					layout="responsive"
-				/>
+			<div className={styles.container}>
+				<div className={styles.logo}>
+					<Image
+						src="/images/logo.svg"
+						alt="Blogr logo"
+						width={85}
+						height={35}
+						layout="responsive"
+					/>
+				</div>
+
+				<ul className={styles.group}>
+					<NavItem text="Product">
+						<Dropdown
+							links={[ 'Overview', 'Pricing', 'Features', 'Integrations' ]}
+						/>
+					</NavItem>
+					<NavItem text="Company">
+						<Dropdown links={[ 'About', 'Team', 'Blog', 'Careers' ]} />
+					</NavItem>
+					<NavItem text="Connect">
+						<Dropdown links={[ 'Contact', 'Newsletter', 'LinkedIn' ]} />
+					</NavItem>
+				</ul>
+
+				<ul className={`${styles.group} ${styles.groupRight}`}>
+					<li>
+						<a>Login</a>
+					</li>
+					<li>
+						<Button text="Sign Up" variant="secondary" />
+					</li>
+				</ul>
+
+				<div className={styles.menuIcon}>
+					<Image
+						src="/images/icon-hamburger.svg"
+						alt="Blogr logo"
+						width={50}
+						height={35}
+						layout="responsive"
+					/>
+				</div>
 			</div>
-
-			<ul className={styles.group}>
-				<NavItem text="Product">
-					<Dropdown links={[ 'Overview', 'Pricing', 'Features', 'Integrations' ]} />
-				</NavItem>
-				<NavItem text="Company">
-					<Dropdown links={[ 'About', 'Team', 'Blog', 'Careers' ]} />
-				</NavItem>
-				<NavItem text="Connect">
-					<Dropdown links={[ 'Contact', 'Newsletter', 'LinkedIn' ]} />
-				</NavItem>
-			</ul>
-
-			<ul className={`${styles.group} ${styles.groupRight}`}>
-				<li>
-					<a>Login</a>
-				</li>
-				<li>
-					<Button text="Sign Up" variant="secondary" />
-				</li>
-			</ul>
 		</nav>
 	);
 };
